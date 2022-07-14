1c492c92633da1ebfda34332060d04d21467e2f2
JINBOK LEE <eyelash1024@naver.com>
2022-07-14 23:10:30 +0900

    login&redirect modified

--- 06_p3_hnm_react_router_practice/src/App.js
+++ 06_p3_hnm_react_router_practice/src/App.js
@@ -3,15 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
-import ProductDetail from "./page/ProductDetail";
 import Navbar from "./component/Navbar";
 import { useState, useEffect } from "react";
+import PrivateRoute from "./route/PrivateRoute";
 
 // 1. 전체 상품 페이지 / login 페이지 / 상품 상세 페이지 제작 예정 ---done
 // 1-1. navbar 만들기 >> 각 component에 만들게 되면 중복이 다수 발생. 따라서, 공통내용은 routes 밖에서 작성! ---done
-// 2. 전체 상품 페이지 에서는 전체 상품을 볼 수 있다
-// 3. login버튼을 누르면, login 페이지가 나온다
-// 4. 상품 디테일을 눌렀으나, login이 안되어 있을 경우, login 페이지가 나온다.
+// 2. 전체 상품 페이지 에서는 전체 상품을 볼 수 있다 ---done
+// 3. login버튼을 누르면, login 페이지가 나온다 ---done
+// 4. 상품 디테일을 눌렀으나, login이 안되어 있을 경우, login 페이지가 나온다. ---done
 // 5. login을 한 상테에서는 상품 디테일 페이지를 볼 수 있다.
 // 6. logout버튼을 누르면 로그아웃 된다
 // 6. 상품 디테일 페이지에서 logout버튼을 누르면 로그아웃 되어 상품 디테일 페이지를 볼 수 없으며, 다시 login 창이 보인다.
@@ -31,7 +31,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProductAll />} />
         <Route path="/login" element={<Login setAuthenticate={setAuthenticate} />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/product/:id" element={<PrivateRoute authenticate={authenticate}/>} />
       </Routes>
     </div>
   );
--- 06_p3_hnm_react_router_practice/src/component/ProductCard.js
+++ 06_p3_hnm_react_router_practice/src/component/ProductCard.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
+  const navigate = useNavigate();
+  const showDetail = () => {
+    navigate(`/product/${item.id}`);
+  };
+
   return (
-    <div className="pb-5 product_card">
+    <div className="pb-5 product_card" onClick={showDetail}>
       <img src={item?.img} alt="" className="product_card_img pb-2" />
       <div className="product_card_detail">
         <div>{item?.choice === true ? "Conscious choice" : ""}</div>

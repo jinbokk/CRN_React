293fe586912c95dd6369447b40bf8e53b579c5ac
JINBOK LEE <eyelash1024@naver.com>
2022-07-14 23:33:58 +0900

    product detail page modified

--- 06_p3_hnm_react_router_practice/src/page/ProductDetail.js
+++ 06_p3_hnm_react_router_practice/src/page/ProductDetail.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { React, useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-  return (
-    <div>상품 상세 페이지</div>
-  )
-}
+  let { id } = useParams();
 
-export default ProductDetail
+  const [product, setProduct] = useState(null);
+
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  };
+
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col className="product_img">
+          <img src={product?.img} />
+        </Col>
+        <Col>
+          <div>{product?.title}</div>
+          <div>{product?.price}</div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductDetail;

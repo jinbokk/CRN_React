f17b835b8334b29cff4a126871975baa98ef58d5
JINBOK LEE <eyelash1024@naver.com>
2022-07-18 18:54:13 +0900

    redux dev tool modified

--- 06_p3_hnm_react_router_practice/src/redux/store.js
+++ 06_p3_hnm_react_router_practice/src/redux/store.js
@@ -1,7 +1,8 @@
 import { legacy_createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 
-let store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+let store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;

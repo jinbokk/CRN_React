78eb32cec119e27ae4d02ee8adad35f68e4bede0
JINBOK LEE <eyelash1024@naver.com>
2022-07-18 19:29:43 +0900

    redux toolkit modified

--- 06_p3_hnm_react_router_practice/src/redux/actions/productAction.js
+++ 06_p3_hnm_react_router_practice/src/redux/actions/productAction.js
@@ -1,10 +1,13 @@
+import { productActions } from "../reducers/productReducer";
+
 function getProducts(searchQuery) {
   return async (dispatch, getState) => {
     let url = `http://localhost:5000/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
-    dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
+    // dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
+    dispatch(productActions.getAllproducts({ data }));
   };
 }
 
--- 06_p3_hnm_react_router_practice/src/redux/store.js
+++ 06_p3_hnm_react_router_practice/src/redux/store.js
@@ -1,8 +1,21 @@
-import { legacy_createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/index";
+// import { legacy_createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers/index";
+import { configureStore } from "@reduxjs/toolkit";
+import authenticateReducer from "./reducers/authenticateReducer";
+import productReducer from "./reducers/productReducer";
 
-let store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+// let store = legacy_createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: {
+    auth: authenticateReducer,
+    product: productReducer,
+  },
+});
 
 export default store;
